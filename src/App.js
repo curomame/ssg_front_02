eb0efed6e5a84b661afbdb441227ad15c77e5d36
ccamanglab <curomame@naver.com>
2022-07-30 20:26:55 +0900

    🐛bug : index.html파일 위치 변경
    React 폴더 설계하다 실행이 안되는 오류를 성범님께 보고받음. 곧바로 수정하였음. 문제의 원인은 react가 읽어들이는 public 과 src의 관계였으며, public에 index.html을 추가해주고 src에서 import 되는 방식을 수정하여 해당 문제를 해결함

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Main from 'pages/main/Main';
+import React from 'react'
+
+function App() {
+
+  return (
+
+      <div className="App">
+        <Main/>
+      </div>
+
+  );
+}
+
+export default App;
---
+++ src/pages/main/Main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div>Main test</div>
+  )
+}
+
+export default Main
