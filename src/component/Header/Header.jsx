a95433131d625fba786ea1d5f0081db646161ffa
ccamanglab <curomame@naver.com>
2022-08-01 14:35:31 +0900

    ➕add : 파트분배 및 폴더 생성
    다들 맡은 파트를 진행하면서, 피드백을 드리겠습니다 :>

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,14 @@
+import Logo from 'component/ui/Logo'
+import SearchBar from 'component/ui/SearchBar'
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+        <Logo/>
+        <SearchBar/>
+    </>
+  )
+}
+
+export default Header
