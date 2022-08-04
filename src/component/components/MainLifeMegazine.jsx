accbc2814adf0df6b8720b243ed90764cc85f074
Jiwon Yu <jwyu1995@naver.com>
2022-08-04 16:08:24 +0900

    ➕add : 0804 16:00 진행
    Main Page Publish

---
+++ src/component/components/MainLifeMegazine.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import MainTitleSub from '../parts/MainTitleSub';
+import MainEventSlider from './MainEventSlider';
+
+function MainLifeMegazine() {
+    return ( 
+        <>
+        <MainTitleSub title="Life Megazine" />
+        {/* 여기에 사용하는게 맞는지 확인 필요 */}
+        <MainEventSlider/>
+        </>
+     );
+}
+
+export default MainLifeMegazine;
--- src/component/components/MainPremiumNewArrivals.jsx
+++ src/component/components/MainPremiumNewArrivals.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import Items from '../layout/Items';
+import MainCategorySelect from '../parts/MainCategorySelect';
 import MainHyperLink from '../parts/MainHyperLink';
 import MainTitleSub from '../parts/MainTitleSub';
 
-
-function MainSpecialGift() {
-
-    // 일정 갯수까지만 반복되는 함수 작성
-
+function MainPremiumNewArrival() {
     return ( 
         <>
-        <MainTitleSub title="Special Gift" sub="포장으로 마음을 담은 백화점 선물"/>
+        <MainTitleSub title="Premium New Arrivals" />
+        <MainCategorySelect/>
         <Items/>
         <MainHyperLink/>
         </>
      );
 }
 
-export default MainSpecialGift;
+export default MainPremiumNewArrival;
