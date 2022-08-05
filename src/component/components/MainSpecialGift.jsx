1310d8b49997fad8e9e83e6d615aa8fd227061cf
Jiwon Yu <jwyu1995@naver.com>
2022-08-05 15:42:15 +0900

    📐Modify : 0805 15:30 진행
    - Modify MainSpecialGift.jsx, Items.jsx for use axios
    - Modify SpecialGist.json for Test

--- src/component/components/MainSpecialGift.jsx
+++ src/component/components/MainSpecialGift.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Items from '../layout/Items';
 import MainHyperLink from '../parts/MainHyperLink';
 import MainTitleSub from '../parts/MainTitleSub';
@@ -6,12 +7,38 @@ import MainTitleSub from '../parts/MainTitleSub';
 
 function MainSpecialGift() {
 
+
+    const [giftDatas,setGiftDatas] = useState([]);
+
     // 일정 갯수까지만 반복되는 함수 작성
+    useEffect(() => {
+        axios.get('http://localhost:5000/gifts')
+            .then(res => {
+                setGiftDatas(res.data);
+                console.log("Success");
+            })
+            .catch((Error) => {
+                console.log("Fail : " + Error);
+            });
+    }, [])
 
     return ( 
         <>
         <MainTitleSub title="Special Gift" sub="포장으로 마음을 담은 백화점 선물"/>
-        <Items/>
+            <div style={{"display":"flex"}}>
+            {
+                giftDatas && giftDatas.map(items => (
+                    <Items 
+                        imgsrc={items.imgsrc}
+                        alt={items.sub}
+                        title={items.title}
+                        sub={items.sub}
+                        price={items.price}
+                        discount={items.discount} />
+            
+            ))}
+        </div>
+
         <MainHyperLink/>
         </>
      );
--- src/component/layout/Items.jsx
+++ src/component/layout/Items.jsx
@@ -1,4 +1,3 @@
-import axios from 'axios'
 import React from 'react'
 import MainTitleSub from '../parts/MainTitleSub'
 import StarNReview from '../parts/StarNReview'
@@ -6,24 +5,26 @@ import StarNReview from '../parts/StarNReview'
 //상품 데이터 받아오기 필요
 //여기서 상품 데이터 받아오기 진행해보기
 
-function Items() {
+function Items({imgsrc, alt, title, sub, price, discount}) {
+  
+//개별 아이템
 
 
   return (
     <>
       <div style={{"display":"flex","flexDirection":"column", "width":"135px"}}>
         <div><img 
-        src="https://sui.ssgcdn.com/cmpt/banner/202207/2022072216460396994225724522_92.png" 
-        alt="상품"
+        src={imgsrc} 
+        alt={alt}
         width="135px"
         height="135px"
         /></div>
         <div>
           <MainTitleSub 
-            title="상품명"
-            sub="상품데이터"
-            price="10000"
-            discount='5'
+            title={title}
+            sub={sub}
+            price={price}
+            discount={discount}
         /></div>
         <div><StarNReview point="3.0" count="3"/></div>
       </div>
