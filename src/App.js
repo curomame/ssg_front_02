ce142497677676d5a7da8573d55a2f3e5ae47698
curomame <curomame@naver.com>
2022-08-08 17:19:43 +0900

    ➕add : 0808 SearchModal Ing
    서치 모달 제작중
    CSS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Test from './component/test/Test';
 import Category from './pages/Category';
 import Login from './pages/Login';
 import Main from './pages/Main';
-// import Pages from './pages/Pages';
+
+import {SearchModalContext} from './context/SearchModalContext'
+import SearchModal from './component/parts/commonsParts/SearchModal';
+
 
 function App() {
 
+  const [isModal, setIsModal] = useState(false);
+
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main/>}/>
-        <Route path="/category" element={<Category/>}/> 
-        <Route path="/login" element={<Login/>}/> 
 
-        {/* <Route path="/page/:id" element={<Pages/>}/> */}
-        <Route path="/test" element={<Test/>}/>
-      </Routes>
+    <SearchModalContext.Provider value={{isModal,setIsModal}}>
+      <SearchModal/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main/>}/>
+          <Route path="/category" element={<Category/>}/> 
+          <Route path="/login" element={<Login/>}/> 
 
-    </BrowserRouter>
+          {/* <Route path="/page/:id" element={<Pages/>}/> */}
+          <Route path="/test" element={<Test/>}/>
+        </Routes>
+      </BrowserRouter>
+
+    </SearchModalContext.Provider>
   );
 }
 
--- src/component/layout/BottomNav.jsx
+++ src/component/layout/BottomNav.jsx
@@ -3,24 +3,26 @@ import { Link } from 'react-router-dom';
 import NavMenu from '../../assets/datas/commonDatas/mainBottomNav.json';
 import SearchModal from '../parts/commonsParts/SearchModal';
 
+
 function BottomNav() {
+
     return (
         <>
-            <div className="bottomNav">
-            <ul>
-                {NavMenu && NavMenu.map(menu => (
-                            <li>
-                                <Link to = {menu.url} key={menu.id}>
-                                    <div><span className="material-icons-outlined bottonNavIconSize">{menu.icon}</span></div>
-                                    <div><span className="bottonNavFont">{menu.title}</span></div>
-                                </Link>
-                            </li>
-                ))}
-            </ul>
+          <div className="bottomNav">
+          <ul>
+              {NavMenu && NavMenu.map(menu => (
+                          <li>
+                              <Link to={menu.url} key={menu.id}>
+                                  <div><span className="material-icons-outlined bottonNavIconSize">{menu.icon}</span></div>
+                                  <div><span className="bottonNavFont">{menu.title}</span></div>
+                              </Link>
+                          </li>
+              ))}
+          </ul>
 
-            </div>
+          </div>
 
-            <SearchModal/>
+            
         </>
     );
 }
