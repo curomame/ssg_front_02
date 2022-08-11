afd2fbb11b2c3d9bda140ad2643e32282b39eab5
Jiwon Yu <jwyu1995@naver.com>
2022-08-11 17:13:11 +0900

    📐Modify : 0811 17:15 진행
    - Add RealTime Password Verify Func.
    - Add Base-Func for Checkbox State Recognition
    - Modify Terms agree Page

--- src/component/components/signup/SignUpTermsBody.jsx
+++ src/component/components/signup/SignUpTermsBody.jsx
@@ -1,36 +1,83 @@
-import React from 'react'
-import SignupButton from '../../parts/signUpParts/SignupButton'
-import SignUpCheckBox from '../../parts/signUpParts/SignUpCheckBox'
+import React, { useState } from "react";
+import SignupButton from "../../parts/signUpParts/SignupButton";
+import SignUpCheckBox from "../../parts/signUpParts/SignUpCheckBox";
 
 function SignUpTermsBody() {
-  return (
-    <>
-    <div>
-    <SignUpCheckBox label="약관 전체 동의" hideoption="Y"/>
-    </div>
-    <br/>
-    <div>
-      <h3>신세계 포인트</h3>
-    <ul>
-        <li><SignUpCheckBox label="(필수) 신세계포인트 회원 이용약관"/></li>
-        <li><SignUpCheckBox label="(필수) 개인정보 수집 및 이용 동의"/></li>
-        <li><SignUpCheckBox label="(필수) 필수 정보 이마트/신세계백화점 공동
-개인정보 수집 이용 동의"/></li>
-      </ul>
-    </div>
-    <br/>
-    <div>
-      <h3>SSG.COM</h3>
-    <ul>
-        <li><SignUpCheckBox label="(필수) SSG.COM 회원 이용약관"/></li>
-        <li><SignUpCheckBox label="(필수) 개인정보 수집 및 이용 등의"/></li>
-      </ul>
-    </div>
+  // SSG Point 약관 Title 및 id 정보
+  const termListSsgPoint = [
+    { idx: 0, title: "(필수) 신세계포인트 회원 이용약관", value: false },
+    { idx: 1, title: "(필수) 개인정보 수집 및 이용 동의", value: false },
+    {
+      idx: 2,
+      title:
+        "(필수) 필수 정보 이마트/신세계백화점 공동 개인정보 수집 이용 동의",
+      value: false,
+    },
+  ];
 
-      <br/>
-      <SignupButton url="../signup/info" title="다음"/>
-    </>
-  )
+  // SSG.COM 약관 Title 및 id 정보
+  const termListSsgCom = [
+    { idx: 3, title: "(필수) SSG.COM 회원 이용약관", value: false },
+    { idx: 4, title: "(필수) 개인정보 수집 및 이용 동의", value: false },
+  ];
+
+  // 약관별 동의 여부 정보 확인
+  const [checkedList, setCheckedList] = useState(new Array(5));
+
+  const handleCheck = (e) => {
+    // setCheckedList([...checkedList, chk.target.value]);
+    // console.log(chk);
+  };
+
+  const handleSend = (e) => {
+    console.log(checkedList);
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSend}>
+        {/* 전체 약관 동의 */}
+        <div>
+          <label>
+            <input type="checkbox" />
+            전체 약관 동의
+          </label>
+        </div>
+
+        <br />
+
+        {/* SSG Point 약관 Title 및 id 정보 */}
+        <div>
+          {termListSsgPoint.map((item, i) => (
+            <label key={i}>
+              <input
+                type="checkbox"
+                value={item.idx}
+                onChange={(e) => handleCheck(e)}
+              />
+              {item.title}
+              <br />
+            </label>
+          ))}
+        </div>
+
+        <br />
+
+        {/* SSG.COM 약관 Title 및 id 정보 */}
+        <div>
+          {termListSsgCom.map((item, i) => (
+            <label key={i}>
+              <input type="checkbox" value={item.idx} onChange={handleCheck} />
+              {item.title}
+              <br />
+            </label>
+          ))}
+        </div>
+        <button type="submit">다음</button>
+      </form>
+    </div>
+  );
 }
 
-export default SignUpTermsBody
+export default SignUpTermsBody;
--- src/component/parts/signUpParts/SignUpInfoField.jsx
+++ src/component/parts/signUpParts/SignUpInfoField.jsx
@@ -1,141 +1,152 @@
-import axios from 'axios'
-import React, { useState } from 'react'
+import axios from "axios";
+import React, { useState } from "react";
 
 function SignUpInfoField() {
+  // 인증 성공 후 받아온 데이터
+  const basicInfo = ["TestUser", "010-1234-5678"];
 
-// 인증 성공 후 받아온 데이터
-const basicInfo = ["TestUser", "010-1234-5678"]
+  const url = "/";
 
-const url = "/"
+  const [signUpData, setSignUpData] = useState({
+    userId: "",
+    password: "",
+    passwordCheck: "",
+    userName: basicInfo[0],
+    address: "",
+    phoneNumber: basicInfo[1],
+    userEmail: "",
+  });
 
-const [signUpData, setSignUpData] = useState({
-  userId: "",
-  password: "",
-  userName: basicInfo[0],
-  address: "",
-  phoneNumber: basicInfo[1],
-  userEmail: ""
-})
+  const handleCheck = (e) => {
+    setSignUpData({ ...signUpData, [e.target.name]: e.target.value });
 
-const handleChange = ( e ) => {
-  console.log(e)
-  setSignUpData(
-      { ...signUpData, [e.target.name]: e.target.value }
-  )
-}
+    if (signUpData.password != signUpData.passwordCheck)
+      console.log("비밀번호 검증 실패(불일치)");
+    else {
+      console.log("비밀번호 검증 성공");
+    }
+  };
 
-const handleSignUp = ( e ) => {
-  console.log(signUpData)
-  e.preventDefault()
+  const handleChange = (e) => {
+    // console.log(e.target.name);
+    setSignUpData({ ...signUpData, [e.target.name]: e.target.value });
+  };
 
-  axios.post(url, {
-      userid : signUpData.userId,
-      password : signUpData.password,
-      userName: basicInfo[0],
-      address: signUpData.address,
-      phoneNumber: basicInfo[1],
-      userEmail: signUpData.userEmail
-  })
-  .then(
-      Response => {
-          console.log(Response)
+  const handleSignUp = (e) => {
+    // console.log(signUpData);
+    e.preventDefault();
+
+    axios
+      .post(url, {
+        userid: signUpData.userId,
+        password: signUpData.password,
+        userName: basicInfo[0],
+        address: signUpData.address,
+        phoneNumber: basicInfo[1],
+        userEmail: signUpData.userEmail,
       })
-      .catch(
-          error => {
-              console.log(error)
+      .then((Response) => {
+        console.log(Response);
       })
-}
-
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div>
-        <form onSubmit={handleSignUp}>
-            {/* 아이디 입력칸 */}
-            <div>
-              <label>
-              아이디
-              <input
-                    type="text"
-                    name="userId"
-                    placeholder="영어 또는 숫자로 6~20자리"
-                    value={signUpData.userId}
-                    onChange={handleChange}
-                    />
-              </label>
-              <button>중복 확인</button>
-            </div>
-            <hr/>
-            {/* 비밀번호 입력칸 */}
-            <div style={{"display": "flex"}}>
-              <label>
-                비밀번호
-                </label>
-                <div>
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="영문, 숫자 조합 8~20자리"
-                  value={signUpData.password}
-                  onChange={handleChange}
-                />
-                <br/>
-                <input
-                  type="password"
-                  name="passwordCheck"
-                  placeholder="비밀번호 재확인"
-                  onChange={handleChange}
-                />
-                </div>
-            </div>
-            <hr/>
-            {/* 이름칸 */}
-            <div>
-              <label>
-                이름
-              </label>
-              {basicInfo[0]}
-            </div>
-            <hr/>
-            {/* 주소 입력칸 */}
-            <div>
-              <label>
-                주소
-                <input
-                  type='text'
-                  name="address"
-                  value={signUpData.address}
-                  onChange={handleChange}
-                />
-                <button>우편번호</button>
-              </label>
-            </div>
-            <hr/>
-            {/* 휴대폰 번호칸 */}
-            <div>
-              <label>
-                휴대폰 번호
-              </label>
-              {basicInfo[1]}
-            </div>
-            <hr/>
-            {/* 이메일 주소칸 */}
-            <div>
-              <label>
-                이메일주소
-              <input
-                  type="text"
-                  name="userEmail"
-                  placeholder="이메일주소"
-                  value={signUpData.userEmail}
-                  onChange={handleChange}
-                />
-              </label>
-            </div>
-            <hr/>
-            <button type="submit" style={{backgroundColor:"#ff5b59", border: 0, outline: 0, color: "#fff"}}>가입하기</button>
-        </form>
+      <form onSubmit={handleSignUp}>
+        {/* 아이디 입력칸 */}
+        <div>
+          <label>
+            아이디
+            <input
+              type="text"
+              name="userId"
+              placeholder="영어 또는 숫자로 6~20자리"
+              value={signUpData.userId}
+              onChange={handleChange}
+            />
+          </label>
+          <button>중복 확인</button>
+        </div>
+        <hr />
+        {/* 비밀번호 입력칸 */}
+        <div style={{ display: "flex" }}>
+          <label>비밀번호</label>
+          <div>
+            <input
+              type="password"
+              name="password"
+              placeholder="영문, 숫자 조합 8~20자리"
+              value={signUpData.password}
+              onChange={handleChange}
+            />
+            <br />
+            <input
+              type="password"
+              name="passwordCheck"
+              placeholder="비밀번호 재확인"
+              value={signUpData.passwordCheck}
+              onChange={handleCheck}
+            />
+          </div>
+        </div>
+        <hr />
+        {/* 이름칸 */}
+        <div>
+          <label>이름</label>
+          {basicInfo[0]}
+        </div>
+        <hr />
+        {/* 주소 입력칸 */}
+        <div>
+          <label>
+            주소
+            <input
+              type="text"
+              name="address"
+              value={signUpData.address}
+              onChange={handleChange}
+            />
+            <button>우편번호</button>
+          </label>
+        </div>
+        <hr />
+        {/* 휴대폰 번호칸 */}
+        <div>
+          <label>휴대폰 번호</label>
+          {basicInfo[1]}
+        </div>
+        <hr />
+        {/* 이메일 주소칸 */}
+        <div>
+          <label>
+            이메일주소
+            <input
+              type="text"
+              name="userEmail"
+              placeholder="이메일주소"
+              value={signUpData.userEmail}
+              onChange={handleChange}
+            />
+          </label>
+        </div>
+        <hr />
+        <button
+          type="submit"
+          style={{
+            backgroundColor: "#ff5b59",
+            border: 0,
+            outline: 0,
+            color: "#fff",
+          }}
+        >
+          가입하기
+        </button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default SignUpInfoField
+export default SignUpInfoField;
