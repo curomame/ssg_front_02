78706a256d0b8177c54e6d1be7b01df07017c61e
Jiwon Yu <jwyu1995@naver.com>
2022-08-12 11:56:15 +0900

    ➕add : 0812 12:00 진행
    - Add Product Detail Page, Components and Parts

---
+++ src/component/parts/productDetailParts/ProductDetailVendorInfo.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function ProductDetailVendorInfo() {
+  return (
+    <div>
+      <div>
+        <p>삼성</p>
+      </div>
+      <div>
+        <p>삼성전자 갤럭시북2 프로 NT950XEV-G51A/12세대/i5-1240P/노트북</p>
+      </div>
+      <div>
+        <p>1,699,000원</p>
+      </div>
+    </div>
+  );
+}
+
+export default ProductDetailVendorInfo;
