711b2f944694b2886456e4f033226badbd947264
RainBeat <jwyu1995@naver.com>
2022-08-17 17:27:16 +0900

    📐Modify : 0817 17:30 진행
    - Modify SignUpInfoField for apply regex check Function
    - Update package for use daum-postcode

--- src/component/parts/signUpParts/SignUpInfoField.jsx
+++ src/component/parts/signUpParts/SignUpInfoField.jsx
@@ -1,6 +1,7 @@
-import axios from "axios";
 import React, { useState } from "react";
 import { useEffect } from "react";
+import { useDaumPostcodePopup } from "react-daum-postcode";
+import { postcodeScriptUrl } from "react-daum-postcode/lib/loadPostcode";
 
 function SignUpInfoField() {
   // 인증 성공 후 받아온 데이터
@@ -24,7 +25,13 @@ function SignUpInfoField() {
   // 입력 값에 대한 제한 조건 검증을 위한 코드
   const handleChange = (e) => {
     // 영문, 숫자 외 값의 입력을 방지하기 위한 정규표현식
-    const regex = /^[a-z0-9]*$/;
+    var regex = /^[a-z0-9]*$/;
+
+    // 이메일 주소 입력 시 적용될 정규표현식과 입력 중일 경우 검증 동작
+    if (e.target.name === "userEmail") {
+      regex = /^[@a-z0-9.]*$/;
+      handleEmailCheck(e.target.value);
+    }
 
     // 입력받은 문자열이 정규표현식을 만족여부를 판별
     if (regex.test(e.target.value)) {
@@ -45,6 +52,15 @@ function SignUpInfoField() {
       console.log("비밀번호 검증 실패(불일치)");
     }
   };
+
+  // 이메일 주소 형식 검증을 위한 코드
+  const handleEmailCheck = (chk) => {
+    // 값이 정상적으로 들어오는 지 확인을 위한 출력
+    // console.log(chk);
+  };
+
+  // 주소 찾기 시작
+  // 주소 찾기 끝.
 
   return (
     <div>
