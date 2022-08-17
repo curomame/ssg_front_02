793cea21aacb29470c9227e86e8fd058f473b35b
curomame <curomame@naver.com>
2022-08-17 16:10:16 +0900

    📐Modify : 0817 16:00 진행
    전체적인 수정 진행중
    현재 카테고리 세부 작업중

---
+++ src/pages/MyPage.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Footer from '../component/components/common/Footer'
+import Header from '../component/components/common/Header'
+import BottomNav from '../component/layout/BottomNav'
+import '../assets/css/mypage.css'
+
+function MyPage() {
+  return (
+    <>
+      <Header
+        type={'mypage'}/>
+
+      <div className='mypageProfile'>
+        <div className='mypageProfileIcon'>
+          <span class="material-icons-outlined">account_circle</span>
+        </div>
+        <div className='mypageProfileDetail'>
+          <div>이형민 님 {'>'}</div>
+          <div><span>FRIENDS</span> 등급입니다.</div>
+        </div>
+      </div>
+
+
+      <div className='mypageFunctions'>
+        <div>
+          <p><span class="material-icons-outlined">favorite_border</span>좋아요</p>
+        </div>
+        
+        <div>
+        <p><span class="material-icons-outlined">local_shipping</span>배송지관리</p>
+        </div>
+        
+        <div>
+        <p><span class="material-icons-outlined">notifications</span>알림함</p>
+        </div>
+        
+        </div>
+
+      <Footer/>
+      <BottomNav/>
+    </>
+  )
+}
+
+export default MyPage
