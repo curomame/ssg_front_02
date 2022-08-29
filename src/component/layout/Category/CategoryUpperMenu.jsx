195a52c6344c600fa1642c36d9ec3aac1f8b0603
curomame <curomame@naver.com>
2022-08-29 18:47:53 +0900

    🐛bug : bugFixing

--- src/component/layout/Category/CategoryUpperMenu.jsx
+++ src/component/layout/Category/CategoryUpperMenu.jsx
@@ -12,49 +12,33 @@ function CategoryUpperMenu() {
   const location = useLocation()
   const navigate = useNavigate();
   const categoryDatas = useRecoilValue(CategoryState);  
+  
 
-  // console.log(categoryDatas);
-
-  const [Lid, setLid] = useState(location.state.Lid ? location.state.Lid : 1)
-  const [Mid, setMid] = useState(location.state.Mid ? location.state.Mid : 1)
+  const [Lid, setLid] = useState(location.state.Lid)
+  const [Mid, setMid] = useState(location.state.Mid)
 
 
   const [LDatas, setLDatas] = useState(null);
   const [MDatas, setMDatas] = useState(null);
 
   useEffect(()=>{
-    
-    // console.log(categoryDatas);
-    console.log(Lid,Mid);
-    console.log(categoryDatas[Lid-1]);
-    console.log(categoryDatas[Lid-1].lcategoryList);
+
     {categoryDatas &&
-      // console.log(categoryDatas[Lid-1],'here1');
-      // console.log(categoryDatas[Lid-1].lcategoryList) 
 
       setLDatas(categoryDatas[Lid-1].lcategoryList);
+      categoryDatas[Lid-1].lcategoryList.filter((item,i)=> {if(item.id===Mid)
+        setMid(i)
+        setMDatas(item.mcategoryList)
+      });
     }
 
-
-    {
-      // categoryDatas &&
-      // console.log(categoryDatas[Lid-1].lcategoryList[Mid-1],'here1'); 
-      
-      // console.log(categoryDatas[Lid-1].lcategoryList.filter((item)=> {if(item.id === Mid) return item[0].mcategoryList}));
-
-      // console.log(categoryDatas[Lid-1].lcategoryList[Mid-1].mcategoryList,'here2');
-      // setMDatas(categoryDatas[Lid-1].lcategoryList.filter((item)=> {if(item.id === Mid) return item}))
-
-    }
-
-
-  },[categoryDatas,Lid,Mid])
+  },[categoryDatas])
 
 
   return (
     <>
 
-    {LDatas && 
+    {MDatas && 
 
     <div className='categoryUpperMenuContainer'>
           
@@ -74,10 +58,8 @@ function CategoryUpperMenu() {
           
             LDatas.map((data,i) => 
             { 
-              // console.log(data.id);
               return <div onClick={()=>{
-                // console.log(data.id)
-                setMid(data.id)}} key={i}>
+                setMid(i+1)}} key={i}>
                     <p>{data.name}</p>
                     </div>}
             )
